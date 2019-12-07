e42ebee701f8a3cb0d62bbf4468a6bf5197a11df
Yash Jain <yash.jain6350@gmail.com>
2019-12-07 16:12:43 +0530

    added recycler view

---
+++ app/src/main/java/com/example/roomandsqlite/dao/NoteDao.java
@@ -0,0 +1,31 @@
+package com.example.roomandsqlite.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.roomandsqlite.entity.Note;
+
+import java.util.List;
+
+// Data acccess objects
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+
+}
